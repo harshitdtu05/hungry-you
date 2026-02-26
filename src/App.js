1a68eb777c42c24014e62cd2278fd1a760539d6b
Harshit <harshit@Harshits-MacBook-Air.local>
2026-02-26 18:20:16 +0530

    quick change

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const Footer = (
     <h6>
       By continuing past this page, you agree to our Terms of Service, Cookie
       Policy, Privacy Policy and Content Policies. All trademarks are properties
-      of their respective owners 2026 © KhaoPiyo™ Ltd. All rights reserved.
+      of their respective owners 2026 © HungryYouuu™ Ltd. All rights reserved.
     </h6>
   </div>
 );
